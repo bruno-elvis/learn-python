5626830630e8165762c6d70ddf1c0f14153e925a
Bruno-Elvis <brunoelvis@outlook.com.br>
2020-11-06 17:37:19 -0300

    Best

--- StartLearnNumPy.py
+++ StartLearnNumPy.py
@@ -7,13 +7,13 @@ npMatriz = np.array(fMatriz)
 
 print()
 print("MATRIZ (ARRAY MULTIDIMENSIONAL) COMUM: \n", str(fMatriz), str(type(fMatriz)))
-print("")
+print()
 print("MATRIZ (ARRAY MULTIDIMENSIONAL) NUMPY: \n", str(npMatriz), str(type(npMatriz)))
-print("")
+print()
 print("ARRAY CONVENCIONAL: ", str(fArray), str(type(fArray)))
-print("")
+print()
 print("ARRAY NUMPY: ", str(npArray), str(type(npArray)))
-print("")
+print()
 
 listaGerada = np.arange(0, 16, 3)
 print("LISTA GERADA DE 0 A 16, COM PASSO DE 3:", listaGerada)
@@ -38,20 +38,20 @@ print("-> MÉTODO RANDOM <-")
 
 arrayDeInteiros = np.random.randint(0, 50, 3) #um array de 0 a 50 contendo 3 índices (submétodo para gerar números inteiros e aleátórios dentro de um intervalo parametrizado)
 
-print(np.random.rand(5)) # 5 índices de números de 0 a 1 (distribuição uniforme)
+print("UMA QUANTIDADE DETERMINADA DE ÍNDICES PASSADA POR PARÂMETRO (5) DISTRIBUIDA UNIFORMEMENTE ENTRE 0 E 1:", np.random.rand(5)) # 5 índices de números de 0 a 1 (distribuição uniforme)
 print()
-print(np.random.rand(5) * 100) # 5 índices de números de 0 a 100
+print("UMA QUANTIDADE DETERMINADA DE ÍNDICES PASSADA POR PARÂMETRO (5) DISTRIBUIDA UNIFORMEMENTE ENTRE 0 E 100:", np.random.rand(5) * 100) # 5 índices de números de 0 a 100
 print()
-print(np.random.randn(5)) # 5 índices de números de -1 a 1 (Distribuição normal (gaussiana) de média 0 e desvio padrão 1 (mais aplicável na prática))
+print("UMA QUANTIDADE DETERMINADA DE ÍNDICES PASSADA POR PARÂMETRO (5) COM DISTRIBUIÇÃO NORMAL DE MÉDIA 0 E DESVIO PADRÃO 1, ENTRE -1 E 1:", np.random.randn(5)) # 5 índices de números de -1 a 1 (Distribuição normal (gaussiana) de média 0 e desvio padrão 1 (mais aplicável na prática))
 print()
-print(arrayDeInteiros) #um array de 0 a 50 contendo 3 índices (submétodo para gerar números inteiros e aleátórios dentro de um intervalo parametrizado)
+print("ARRAY COM INTEIROS ALEATÓRIOS DE 0 a 50, CONTENDO 3 ÍNDICES:\n", arrayDeInteiros) #um array de 0 a 50 contendo 3 índices (submétodo para gerar números inteiros e aleátórios dentro de um intervalo parametrizado)
 print()
-print(np.shape(arrayDeInteiros)) #saber o tamanho em (colunas, linhas) de um array do numpy
+print("RETORNA O TAMANHO EM (COLUNAS, LINHAS) DE UM ARRAY NUMPY (arrayDeInteiros):", np.shape(arrayDeInteiros)) #saber o tamanho em (colunas, linhas) de um array do numpy
 print()
-print("Máximo: " + str(arrayDeInteiros.max()))
-print("Mínimo: " + str(arrayDeInteiros.min()))
-print("Índice que se encontra o número máximo: " + str(arrayDeInteiros.argmax()))
-print("Índice que se encontra o número mínimo: " + str(arrayDeInteiros.argmin()))
+print("INTEIRO MÁXIMO DO ARRAY (arrayDeInteiros):", str(arrayDeInteiros.max()))
+print("INTEIRO MÍNIMO DO ARRAY (arrayDeInteiros):", str(arrayDeInteiros.min()))
+print("ÍNDICE QUE SE ENCONTRA O INTEIRO MÁXIMO (arrayDeInteiros):", str(arrayDeInteiros.argmax()))
+print("ÍNDICE QUE SE ENCONTRA O INTEIRO MÍNIMO (arrayDeInteiros):", str(arrayDeInteiros.argmin()))
 print()
 print()
 # INDEXAÇÃO EM NUMPY ARRAYS
@@ -59,7 +59,8 @@ indArray = np.arange(0, 56, 10)
 print(indArray)
 print()
 #buscado índices dentro de um array numpy
-print(indArray[5])
-print(indArray[2:5])
+print("BUSCANDO ÍNDICE NO ARRAY PASSANDO O ÍNDIDE NO PARÂMENTRO (indArray):", indArray[5])
+print("BUSCANDO ÍNDICES NO ARRAY PASSANDO OS ÍNDIDES DO INTERVALO NO PARÂMENTRO (indArray):", indArray[2:5])
+
 
 # ESTUDAR ANÁLISE EXPORÁTÓRIA #
