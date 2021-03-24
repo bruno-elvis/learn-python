b3911d16ef6abce30cddb67b089427901b5bbd40
Bruno-Elvis <brunoelvis@outlook.com.br>
2021-03-24 10:14:47 -0300

    Finish initiation :)

--- LearnPythonStart.py
+++ LearnPythonStart.py
@@ -4,33 +4,33 @@ print(5*5)
 """
 Comentário de Várias Linhas """ """
 """
-# DEFINIÇÕES DE ALGUMAS VARIÁVEIS E LISTAS
+# DEFINIÇÕES DE ALGUMAS VARIÁVEIS E LISTAS #
 a = 5
 b = 10
 c = "Maior"
 ListaT = [1, 5, 10, 13, 22, 55]
 ListaN = ["Bruno", "Elvis", "Pereira", "Silva"]
 
-# TRATAMENTO DE LISTAS
+# TRATAMENTO DE LISTAS #
 print("TRATAMENTO DE LISTAS")
 
-# UTILIZANDO IF
+# UTILIZANDO IF #
 print("UTILIZANDO IF")
 if a > b:
     print(c)
 else: #também pode ser utilizado o 'elif' cujo objetivo é ser mais uma condição para ser testada antes de continuar a execução, sendo que só irá cair no 'else' se o 'if' e 'elif' forem falsos
     print("Menor")
-    print("")
+    print()
 
-# UTILIZANDO WHILE
+# UTILIZANDO WHILE #
 print("UTILIZANDO WHILE")
 while a > 0:
     print(a)
     a -= 1 #a = a + 1 (outra forma de se escrever a incrementação)
 
-print("")
+print()
 
-# FORMATAÇÃO DE OUTPUT's COM WHILE (SAÍDAS)
+# FORMATAÇÃO DE OUTPUT's COM WHILE (SAÍDAS) #
 print("FORMATAÇÃO DE OUTPUT's COM WHILE (SAÍDAS)")
 ii = 0
 xx = 0
@@ -42,35 +42,35 @@ while (ii < 5) and (xx < 5):
     ii = ii + 1
     xx += + 1
 
-print("")
+print()
 
 zz = 0
 while zz < 5:
     print(f"zz vale agora: {zz}") #é o mesmo que esta notação: " print("z vale agora: {}".format(zz)) "
     zz = zz + 1
 
-print("")
+print()
 
 Saida1 = "Teste S. 001"
 Saida2 = "Teste S. 002"
 print("Aqui está o valor da variável 'T1': {} e aqui está o valor da variável 'T2': {}".format(Saida1, Saida2))
 print("Aqui está o valor da variável 'T1': {T1} e aqui está o valor da variável 'T2': {T2}".format(T1=Saida2, T2=Saida1)) #exemplo¹ utilizando alias com método "format"
 
-print("")
+print()
 
-# FOR E CLASSE 'RANGE'
+# FOR E CLASSE 'RANGE' #
 print("FOR E CLASSE 'RANGE'")
 for i in ListaN, ListaT: #pode ser passado uma quantidade ilimitada de objetos para percorrer
     print(i)
 
-print("")
+print()
 print("RANGE DE TAMANHO 15, INICIADO EM 0 E INCREMENTANDO EM 3")
 for i in range(0, 15, 3): #range(início da lista, final da lista, incrementador (passo da contagem))
     print(i)
 
-print("")
+print()
 
-# PERCORRENDO STRINGS
+# PERCORRENDO STRINGS #
 print("RETORNA O TAMANHO DO ÍNDICE DENTRO DE UM OBJETO (STRING OU LISTA)")
 print("Tamanho da String 'c': " + str(len(c))) #retorna o número de índices em um objeto/container/string/lista
                                                #desconsiderando o '0' como sendo o início da lista (inicia em 1-x)
@@ -79,9 +79,9 @@ print("Tamanho do Índice da ListaT (Objetos da Lista): " + str(len(ListaT)))
 sizeString = len(ListaN)
 print("Tamanho da listaN (Quantidade de Objetos): " + str(sizeString))
 #também retorna o tamanho de uma string (inclui os 'espaços' na quantidade total dos índices)
-print("")
+print()
 
-# NAVEGAÇÃO POR ÍNDICES
+# NAVEGAÇÃO POR ÍNDICES DE LISTAS #
 print("NAVEGAÇÃO POR ÍNDICES")
 
 print("Objeto no índice '1' da listaN: " + ListaN[1]) #no Python a indexação começa em '0'
@@ -90,24 +90,24 @@ print("Caractere no índice '2' da string 'c': " + c[2])
 varMeuNome = "Bruno Elvis Pereira Silva"
 print ("Trexo exibido começando do índice 6 (inclusivo) até o 11 (excludente): " + varMeuNome[6:12]) #exibe o trecho entre índices [X :(à) Y], recebe = (string[índice incial : índice final] e se denomina "slice" este método de pegar uma parte de uma string
                        #sendo que nesta indexação o primeiro índice [X:] é inclusivo (conta com ele) e o segundo índice [:Y] é excludente (não considera ele na exibição)
-print("")
+print()
 print("Formatação em Caixa-Alta do Objeto no índice '0' da ListaN: " + ListaN[0].upper()) #formatação em caixa alta
 print("Formatação em Caixa-Baixa do Objeto no índice '2' da ListaN: " + ListaN[2].lower()) #formatação em minúsculas
 
-print("")
+print()
 
-# FUNÇÕES PARA TRATAMENTO DE STRINGS
+# FUNÇÕES PARA TRATAMENTO DE STRINGS #
 print("TRABALHANDO COM TRATAMENTO DE STRINGS (FUNÇÕES)")
 print("-------------------------------------------------------------------------------------------------")
 
 print("Função 'strip' - varMeuNome: " + varMeuNome.strip()) #remove caracteres especiais ou espaços iniciais e finais de uma string
 print("Função 'split' - varMeuNome: ")
 print(varMeuNome.split(" ")) #separa o conteúdo de uma string em partes, passando um delimitador como parâmetro / é Case Sensitive
-print("")
+print()
 print("Buscar índice do primeiro caractere da string informada no parâmetro (função 'find'): " + str(varMeuNome.find("Silva"))) #(retorna o índice(posição) do primeiro caractere do parâmetro passado para a busca) / busca um caracter ou conjunto de caracteres dentro de uma string (considerando espaços), é Case-sensitive, caso não encontre o parâmetro definido retorna "-1" e não pode ser chamado em listas
 busca = varMeuNome.find("Elvis")
 print("Função 'find' passando como parâmetro a variável com o valor de busca: "+ varMeuNome[busca:]) #exibe o texto iniciado numa posição (var = busca) e finalizado em outra posição (sem parámetro de fim = null)
-print("")
+print()
 print(varMeuNome.replace("Pereira", "Winchester")) #substitui um trecho ou caractere de uma string, o primeiro parâmetro do método é a busca na string e o segundo é o conjunto de caracteres a bustituir
 print("Função 'replace' substititui um valor por outro dentro de uma string: " + varMeuNome)
 
@@ -133,9 +133,9 @@ print(tipoTUPtoLST)
 print(tipoNUMtoSTR)
 
 print("--------------------------------")
-print("")
+print()
 
-# DEFINIÇÂO DE FUNÇÕES
+# DEFINIÇÂO DE FUNÇÕES #
 print("TRABALHANDO COM FUNÇÕES")
 
 def somar(a, b):
@@ -149,7 +149,7 @@ def mult(a, b):
 print("Chamando a função 'somar' passando dois inteiros (5, 10): " + str(somar(5, 10)))
 print("Chamando a função 'mult' passando dois inteiros (5, 5): " + mult(5, 5))
 
-print("")
+print()
 
 print("RETORNO DE FUNÇÕES ESTATICAMENTE TIPADAS")
 def soma (a: int, b: float) -> str: #função com parâmetros tipados (estaticamente tipados)
@@ -157,7 +157,7 @@ def soma (a: int, b: float) -> str: #função com parâmetros tipados (estaticam
 
 print("Chamando a função 'soma' passando dois valores tipados (5, 5.5)" + " = " + soma(5, 5.5))
 
-print("")
+print()
 
 # MANIPULAÇÃO DE ARQUIVOS #
 print("TRABALHANDO COM MANIPULAÇÃO DE ARQUIVOS")
@@ -173,7 +173,7 @@ print("TRABALHANDO COM MANIPULAÇÃO DE ARQUIVOS")
 'a+' -> Leitura e escrita (abre o arquivo para atualização);
 'x'  -> Abre o arquivo para criação exclusiva, falhando se o arquivo já existir.
 """
-insArquivo = open('CONTAS.TXT', 'rt')
+#insArquivo = open('CONTAS.TXT', 'rt')
 #print(insArquivo.read()) #função para ler o arquivo completo no seu formato atual (numa única string)
 
 
@@ -181,72 +181,75 @@ insArquivo = open('CONTAS.TXT', 'rt')
 #linhas = insArquivo.readlines() #função para ler e retornar todas as linhas do arquivo (retorna uma lista)
 #print(linhas)
 
-insArquivo.close() #fecha o arquivo (encerra a instância do objeto manipulador de arquivos)
+#insArquivo.close() #fecha o arquivo (encerra a instância do objeto manipulador de arquivos)
 
 #newFile = open('TESTE_X.TXT', 'w') #cria um novo arquivo (parâmentro 'w' para modo de escrita)
 
 #Para editar o arquivo:
 #newFile.write("Teste de edicao de arquivo")
-print("")
+print()
 
-# TRABALHANDO COM DICIONÁRIOS
+# TRABALHANDO COM DICIONÁRIOS #
 print("TRABALHANDO COM DICIONÁRIOS")
-print("")
+print()
 
-meuDic = {1: "Valor 1", 2: "Valor 2", 3: "Valor 3", "Quarto": "Valor 4"}
+meuDic = {1: "Valor 1", 2: "Valor 2", 3: "Valor 3", 'Quarto': "Valor 4"}
 
 print("meuDic: " + str(meuDic)) #exibe todo o conteúdo do dicionário
-print("")
-print("Valor de um índice especificado parametricamente: " + meuDic['Quarto']) #exibe valor de um índice especificado pela chave via parâmetro
+print()
+print("Valores de um índices especificados parametricamente: " + meuDic['Quarto'] + " / " + meuDic[2]) #exibe valor de um índice especificado pela chave via parâmetro
 
-print("")
+print()
 
 print("CONTEÚDO DO DICIONÁRIO (RETORNA SOMENTE AS CHAVES)")
-for chave in meuDic: #retorna somente as chaves do dicionário
-    print(chave)
+for i in meuDic: #retorna somente as chaves do dicionário
+    print(i)
 
-print("")
+print()
 
 print("VALORES DO DICIONÁRIO (RETORNA EM TUPLAS)")
-for i in meuDic.items(): #percorrendo o dicinário (o método "items() retorna as tuplas com as chaves e valores do dicionário")
-    print(i)
+for itens in meuDic.items(): #percorrendo o dicinário (o método "items() retorna as tuplas com as chaves e valores do dicionário")
+    print(itens)
 
-print("")
+print()
 
-print("CHAVES DO DICIONÁRIO (RETORNA AS CHAVES ESPECIFICAMENTE)")
-for i in meuDic.keys(): #retorna as chaves de um dicionário através da função "keys()"
-    print(i)
+print("CHAVES DO DICIONÁRIO (RETORNA AS CHAVES ESPECIFICAMENTE (Implementado concatenação com os valores))")
+for chaves in meuDic.keys(): #retorna as chaves de um dicionário através da função "keys()"
+    print(chaves)
 
-print("")
+print()
 
 print("VALORES DO DICIONÁRIO (RETORNA OS VALORES)")
-for i in meuDic.values(): #retornando os valores do dicionário através do método "values()"
-    print(i)
+for valores in meuDic.values(): #retornando os valores do dicionário através do método "values()"
+    print(valores)
 
-print("")
+print()
 
 print("CHAVES E VALORES DO DICIONÁRIO CONCATENADOS")
 for chave in meuDic: #exibindo chave + valor concatenados
-    print(str(chave) + " - " + meuDic[chave])
+    print(str(chave) + " = " + meuDic[chave])
 
-print("")
+print()
 
-# CLASSE 'RANDOM'
+# CLASSE 'RANDOM' #
 print("TRABALHANDO CLASSE 'RANDOM'")
 
-import random
-from typing import cast
+import random as rd
+#from typing import KeysView, cast
 
-num = random.randint(1, 15) #método para gerar um número aleatório com parâmetro inicial e final
+num = rd.randint(1, 15) #método para gerar um número aleatório com parâmetro inicial e final
 print("Número aleatório gerado pelo método 'randint' da classe 'random' (de 1 a 15): " + "(" + str(num) + ")")
-print("")
+print()
 
 mynums = [1, 10, 5, 25, 15, 13, 55]
-num = random.choice(mynums) #o método 'choice' da classe random escolhe um item de uma lista de itens
-print("Número escolhido pela função 'choice' na lista 'mynums': " + str(num))
-print("")
+num = rd.choice(mynums) #o método 'choice' da classe random escolhe um item de uma lista de itens
 
-# TRATAMENTO DE EXCEÇÕES
+
+print("Número escolhido pela função 'choice' na lista de inteiros 'mynums': " + str(num))
+print("Número escolhido pela função 'choice' na lista de strings 'ListaN': " + str(rd.choice(ListaN)))
+print()
+
+# TRATAMENTO DE EXCEÇÕES #
 print("TRABALAHNDO COM EXCEÇÕES")
 
 try:
@@ -258,40 +261,43 @@ else:
 finally:
     print("10 / 2 = " + str(10 / 2))
 
-# RECEBENDO VALORES PELO MÉTODO 'INPUT'
+# RECEBENDO VALORES PELO MÉTODO 'INPUT' #
+"""
 print("RECEBENDO VALORES PELO MÉTODO 'INPUT'")
-print("")
+print()
 meu_texto = input("Digite um texto: ") #recebendo textos
-numero_inteiro = int(input("Digite um numero inteiro: "))#recebendo números
-numero_decimal = float(input("Digite um numero decimal: "))#recebendo números
-print(meu_texto + " - " + str(numero_inteiro) + " - " + str(numero_decimal))
-print("")
-print("")
+numero_inteiro = int(input("Digite um numero inteiro: ")) #recebendo inteiros
+numero_decimal = float(input("Digite um numero decimal: ")) #recebendo decimais
+print("Imputs concatenados: " + meu_texto + " | " + str(numero_inteiro) + " | " + str(numero_decimal))
+print()
+"""
+print()
 
-# TRABALHANDO COM SET'S (CONJUNTOS)
+# TRABALHANDO COM SET'S (CONJUNTOS) #
 print("TRABALHANDO COM SET'S (CONJUNTOS)")
-print("")
+print()
 """
 Conjuntos não possuem noção de ordem por isso seus elementos não podem ser acessados com colchetes [] nem podem ser fatiados.
 Os conjuntos (set) não aceitam valores repetidos ao tentar criar um conjunto com valores repetidos eles serão descartados só sobrando um valor do mesmo.
 Aceita tipos diferentes como valor (inteiro, flutuante, tupla, string, etc.), mas mão aceita conjuntos mutáveis (listas ou dicionários) como valor.
- """
-
+"""
 listaSet = {'Maçã', 'Laranja', 'Uva', 'Abacaxi', 'Maçã', 'Abacate', 'Laranja'} #definindo uma variável do tipo 'set' (um conunto)
 setTratado = set(listaSet)
-print(setTratado, str(type(listaSet)))
+print("Exibindo o conjunto (set) 'listaSet': " + str(listaSet) + " | " + str(type(listaSet)))
+print("Exibindo o conjunto (set) 'setTratado' tratado: " + str(setTratado) + " | " + str(type(setTratado)))
+print()
 
 print("Percorrendo o Set tratado pela classe 'set': ")
-for sseett in setTratado:
-    print(sseett)
-print("")
+for set in setTratado:
+    print(set)
+print()
 
 print("Percorrendo o set declarado: ")
 for s in listaSet:
     print(s)
-print("")
+print()
 
-# MOSTRANDO O TIPO DE UM OBJETO
+# RETORNANDO O TIPOS DE OBJETOS #
 tipObjA = type(a)
 tipObjC = type(c)
 tipObjT = type((1, 2, 3))
